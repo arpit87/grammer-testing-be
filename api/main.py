7311a1c49900e7c4f60595e25b3b131f2dc55b15
nkraj7266 <nkraj7266@gmail.com>
2024-06-25 16:19:01 +0530

    updated pipfile

--- api/main.py
+++ api/main.py
@@ -60,8 +60,8 @@ ensure_spacy_model()
 # Setup Gramformer
 def set_seed(seed):
     torch.manual_seed(seed)
-    # if torch.cuda.is_available():
-    #     torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
 set_seed(1212)
 gf = Gramformer(models=1, use_gpu=False)  # 1=corrector, 2=detector
